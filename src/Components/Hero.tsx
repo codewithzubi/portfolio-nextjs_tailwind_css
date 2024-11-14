662b6bebbb9b63925a39851267628181b20eca6b
codewithzubi <159233893+codewithzubi@users.noreply.github.com>
2024-11-14 10:39:12 -0800

    Update Hero.tsx

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import { FaFacebook, FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
-
 const Hero = () => {
   return (
     <div>
