656f0fa51cd08c0bafd9f3f8a496d46b597575c0
codewithzubi <za8089006@gmail.com>
2024-11-14 10:33:05 -0800

    Updated

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,22 +1,43 @@
-import React from "react"
+"use client";
+
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-       <header className="bg-[#170550] p-4 flex justify-around items-center">
-         <div className="text-2xl font-bold">Zubair</div>
-         <nav className="hidden md:flex space-x-4">
-          <a href="/" className="hover:text-orange-500">Home</a>
-          <a href="/About" className="hover:text-orange-500">About Me</a>
-          <a href="Services" className="hover:text-orange-500">Services</a>
-          <a href="/Skill" className="hover:text-orange-500">Skills</a>
-          <a href="/Projects" className="hover:text-orange-500">Projects</a>
-          <a href="/Contact" className="hover:text-orange-500">Contact</a>
-         </nav>
-         <button className="bg-white text-black px-6 py-1 rounded-full font-bold hover:bg-orange-500 transition">Hire me</button>
+      <header className="bg-[#170550] p-4 flex justify-evenly items-center">
+        {/* Logo */}
+        <div className="text-2xl font-bold text-white">Zubair</div>
+
+        {/* Navigation Links */}
+        <nav className="hidden md:flex space-x-4">
+          <a href="/" className="text-white hover:text-orange-500">
+            Home
+          </a>
+          <a href="/About" className="text-white hover:text-orange-500">
+            About Me
+          </a>
+          <a href="/Services" className="text-white hover:text-orange-500">
+            Services
+          </a>
+          <a href="/Skill" className="text-white hover:text-orange-500">
+            Skills
+          </a>
+          <a href="/Projects" className="text-white hover:text-orange-500">
+            Projects
+          </a>
+          <a href="/Contact" className="text-white hover:text-orange-500">
+            Contact
+          </a>
+        </nav>
+
+        {/* Hire Me Button */}
+        <button className="bg-white text-black px-6 py-1 rounded-full font-bold hover:bg-orange-500 transition">
+          Hire me
+        </button>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
